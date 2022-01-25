3e3c02d4770d2591c268053ce985127c3549138a
Vera <Vera.Sipovich@atechart-group.com>
2022-01-25 12:02:44 +0300

    change serializer

--- itechart_project/companies/serializers.py
+++ itechart_project/companies/serializers.py
@@ -4,14 +4,6 @@ from .models import *
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-    # name = serializers.CharField(max_length=MaxLenght.MAX_EMPLOYEE_NAME_LENGTH)
-    # surname = serializers.CharField(max_length=MaxLenght.MAX_EMPLOYEE_SURENAME_LENGTH)
-    # job_position = serializers.CharField(max_length=MaxLenght.MAX_EMPLOYEE_JOB_POSITION_LENGTH)
-    # is_manager = serializers.BooleanField()
-    # is_admin = serializers.BooleanField()
-    # phone_number = serializers.PositiveIntegerField(unique=True)
-    # comp_id = serializers.ForeignKey()
-    # personal_data_id = serializers.OneToOneField()
 
     # serialize one to one relationship
     # date_of_birth = serializers.DateField(source='personal_data_id.date_of_birth')
@@ -39,12 +31,7 @@ class BankSerializer(serializers.ModelSerializer):
 
 
 class CompanySerializer(serializers.ModelSerializer):
-    # name = serializers.CharField(max_length=MaxLenght.MAX_COMPANY_NAME_LENGTH, unique=True)
-    # web_site = serializers.URLField(max_length=MaxLenght.MAX_COMPANY_WEBSITE_LENGTH)
-    # email = serializers.EmailField(max_length=MaxLenght.MAX_COMPANY_EMAIL_LENGTH, null=True)
-    # post_index = serializers.PositiveIntegerField()
     # logo = serializers.ImageField(required=True)
-    # bank_id = serializers.ManyToManyField('Bank')
 
     # employee_id = EmployeeSerializer(many=True)
     employee_id = serializers.CharField(source='employee_id.employee_id')
@@ -68,3 +55,15 @@ class PersonalDataSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return PersonalData.objects.create(**validated_data)
+
+
+class EmployeeWithPersonalData(EmployeeSerializer):
+
+    class Meta:
+        model = PersonalData
+        fields = ['date_of_birth', 'home_address', 'salary', 
+                  'time_create', 'time_update']
+
+    def create(self, validated_data):
+        return PersonalData.objects.create(**validated_data)
+    
