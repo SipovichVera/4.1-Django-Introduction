61c739f5ee86a73f00fcae8f155c1f04f1c5e94e
Vera <Vera.Sipovich@atechart-group.com>
2022-02-03 12:43:18 +0300

    non-working pytest

--- itechart_project/users/tests.py
+++ itechart_project/users/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+# from django.test import TestCase
+# import pytest
+# from django.contrib.auth import authenticate
 
-# Create your tests here.
+# from .models import User
+
+
+# # @pytest.fixture
+# # def user(db) -> User:
+# #     return User.objects.create("vera")
+
+
+# # def test_user_password(db, user: User) -> bool:
+# #     user.set_password(1234)
+# #     assert user.check_password(1234) is True
+
+# # def test_create_category(db):
+# #     category = User.objects.create(username="vera")
+# #     assert category.username == "vera"
+
+
+# class RegistrTest(TestCase):
+
+#     @classmethod
+#     def setUpTestData(cls):
+#         #Set up non-modified objects used by all test methods
+#         User.objects.create(username='vera', password='1234')
+
+#     def test_create_user(self):
+#         self.assertEqual(User.objects.count(), 1)
+#         self.assertEqual(User.objects.get().username, 'vera')
+#         self.assertEqual(User.objects.get().password, '1234')
+
+
+# class LoginTest(TestCase):
+
+#     def setUp(self):
+#         self.user = User.objects.create(username='vera', password='1234')
+#         self.user.save()
+
+#     def test_correct(self):
+#         user = authenticate(username='vera', password='1234')
+#         self.assertTrue(user is not None and user.is_authenticated)
