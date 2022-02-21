b315df4c73966ecde252244bae5d6dba40c34382
Vera <Vera.Sipovich@atechart-group.com>
2022-02-21 17:03:41 +0300

    heroku

--- itechart_project/itechart_project/settings.py
+++ itechart_project/itechart_project/settings.py
@@ -18,8 +18,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -100,6 +98,8 @@ DATABASES = {
 }
 
 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
